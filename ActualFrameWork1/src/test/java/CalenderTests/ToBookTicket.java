5c141b9d749ed0cecd69f4e3461260600516b858
Sabita <Sabita@Sabita-PC.lan>
2025-02-09 21:13:02 +0530

    commit

--- ActualFrameWork1/src/test/java/CalenderTests/ToBookTicket.java
+++ ActualFrameWork1/src/test/java/CalenderTests/ToBookTicket.java
@@ -3,7 +3,7 @@ package CalenderTests;
 public class ToBookTicket {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Book tickets today");
 
 	}
 
